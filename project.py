3039f371af628ce5ad27d5da4da6c0196f6e5e61
Nicolas Dupont <nicolas.dupont@efrei.net>
2024-06-12 19:07:26 +0200

    Update du clean text (quentin) + Tokenize + Stemming + Vectorisation

--- project.py
+++ project.py
@@ -13,12 +13,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import re
 from cleantext import clean 
+from nltk.tokenize import (
+    TweetTokenizer  
+)
+from nltk.stem import WordNetLemmatizer 
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+from wordfreq import word_frequency
+
+import nltk
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('stopwords')
 
 #%% Chargement de la base de données avec tri et nettoyage
 
 try:
     # Lecture du fichier CSV
-    data = pd.read_csv("EFREI - LIPSTIP - 50k elements EPO.csv")
+    data = pd.read_csv("C:\\Users\\nicol\\OneDrive\\Bureau\\Documents\\L3\\s6\\Master Camp\\Project-Explain\\EFREI - LIPSTIP - 50k elements EPO.csv")
     #data = pd.read_csv(r"C:\Users\nicol\OneDrive\Bureau\Documents\L3\s6\Master Camp\Project Explain\base_de_donnees.csv")
     
     # Afficher les premières lignes du DataFrame pour vérifier la lecture
@@ -30,7 +43,7 @@ except Exception as e:
     print("Erreur lors de la lecture du fichier CSV :", e)
     data = "error..."
 
-#%%
+#%% Traitement des données
 #Suppression des doublons
 print("Taille avant :",data.shape)
 data = data.drop_duplicates()
@@ -40,38 +53,98 @@ print("Taille après :",data.shape)
 print(data.isnull().all())
 
 #Retirer les 4 premières colonnes
-cols_to_mantain = ['CPC', 'IPC', 'claim', 'description']
+cols_to_mantain = ['CPC', 'description']
 data = data[cols_to_mantain]
 print(data.shape)
 
 #%%¨Nettoyage des données (balises):
     
-balise_EPO_regex = r'<!--.*?-->'
-balise_xml_regex = r'<[^>]+>'
-number_regex = r'\d+[a-z]*'
-reference_regex1 = r'\(([0-9]*[a-z]*|[ivxlcdm]*)\)'
-reference_regex2 = r'\(\s*(\d+[a-z]*)(\s*,\s*\d+[a-z]*)*(\s*to\s*\d+[a-z]*)?\s*\)'
-special_characters_regex = r'[^a-zA-Z0-9\s<>]'
-
+# Combined regex for faster processing
+combined_regex = re.compile(
+    r'<!--.*?-->|<[^>]+>|\(([0-9]*[a-z]*|[ivxlcdm]*)\)|\(\s*(\d+[a-z]*)(\s*,\s*\d+[a-z]*)*(\s*to\s*\d+[a-z]*)?\s*\)|\d+[a-z]*|[^a-zA-Z0-9\s<>]'
+)
 
 # Function to clean descriptions
 def clean_description(desc):
-    desc = clean(desc, lower=True, no_currency_symbols = True, replace_with_currency_symbol = "<CUR>", lang="en") 
-    desc = re.sub(balise_EPO_regex, '', desc)  # Remove comment tags
-    desc = re.sub(balise_xml_regex, '', desc)  # Remove XML tags
-    desc = re.sub(reference_regex1, '<REF>', desc) 
-    desc = re.sub(reference_regex2, '<REF>', desc) 
-    desc = re.sub(number_regex, ' <NUMBER>', desc) 
-    desc = re.sub(special_characters_regex, '', desc)
+    desc = clean(desc, lower=True, no_currency_symbols=True, replace_with_currency_symbol="<CUR>", lang="en")
+    desc = combined_regex.sub(' ', desc)
     return desc
     
 
 #Test sur les 4 premières ligens car c'est bcp trop long sur les 50000
 data.loc[:4, 'description'] = data.loc[:4, 'description'].apply(clean_description)
-    
+
 print(data['description'][3])
 
-  
+#%%¨Tokenize :
+vocab = set()
+tokenizer = TweetTokenizer()
+
+for _, rev in data.loc[:4, 'description'].items():
+    vocab.update(tokenizer.tokenize(rev))
+print(len(vocab))
+
+# for 1 description
+desc = set()
+tokenizer = TweetTokenizer()
+
+for _, rev in data.loc[:1, 'description'].items():
+    desc.update(tokenizer.tokenize(rev))
+print(len(desc))
+
+vocab = desc -set(stopwords.words('english')) 
+print(len(desc))
+
+
+vocab = vocab -set(stopwords.words('english')) 
+print(len(vocab))
+
+#%% Stemming (enlève la terminaison) and Lemmatization (renvoie à la forme radicale):
+stemmer = PorterStemmer() #shorter
+
+stem_vocab = [stemmer.stem(word) for word in vocab]
+print(stem_vocab)    
+
+stem_vocab = set(stem_vocab)
+print(len(stem_vocab))
+
+# Stemming d'une description
+stem_desc = [stemmer.stem(word) for word in desc]
+print(stem_desc)    
+
+stem_desc = set(stem_desc)
+print(len(stem_desc))
+"""
+lemmatizer = WordNetLemmatizer()
+lemmatizer_vocab = [lemmatizer.lemmatize(word) for word in vocab]
+print(lemmatizer_vocab)
+
+lemmatizer_vocab = set(lemmatizer_vocab)
+print(len(lemmatizer_vocab))
+"""
+
+#%% Vectorisation
+vocabulary = dict()
+vocab_index=0
+
+for word in stem_vocab:
+    if word not in vocabulary:
+        vocabulary[word] = vocab_index
+        vocab_index +=1
+print(vocabulary)
+
+
+
+def vectorize_with_freq(text,vocab):
+    vect = np.zeros(len(vocab))
+    cpt=0
+    for word in text:
+        vect[vocab[word]] +=1
+        cpt +=1
+    vect /=cpt
+    return vect
+print(vectorize_with_freq(stem_desc,vocabulary))
+
 #%% Algorithme supervisé : Classification
 
 #%% Main
